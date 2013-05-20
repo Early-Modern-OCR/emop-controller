2980d7f5a5513b44437047070dca42333e46f2dd
Lou Foster <lou@performantsoftware.com>
2013-05-20 10:23:16 -0400

    better cmd line opts to controll / configure emop controller

--- src/main/java/edu/tamu/emop/EmopController.java
+++ src/main/java/edu/tamu/emop/EmopController.java
@@ -6,7 +6,19 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Properties;
+import java.util.Set;
+import java.util.TreeSet;
 
+import org.apache.commons.cli2.Argument;
+import org.apache.commons.cli2.CommandLine;
+import org.apache.commons.cli2.Group;
+import org.apache.commons.cli2.Option;
+import org.apache.commons.cli2.OptionException;
+import org.apache.commons.cli2.builder.ArgumentBuilder;
+import org.apache.commons.cli2.builder.DefaultOptionBuilder;
+import org.apache.commons.cli2.builder.GroupBuilder;
+import org.apache.commons.cli2.commandline.Parser;
+import org.apache.commons.cli2.validation.EnumValidator;
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.Level;
@@ -29,37 +41,44 @@ import edu.tamu.emop.model.PageImage;
  * 
  * This process depends upon environment variables and .my.cnf to execute properly.
  * Expected environment:
- *    EMOP_WALLTIME     - max time this process will live
- *    JUXTA_HOME        - base directory of juxtaCL install
+ *    JUXTA_HOME         - base directory of juxtaCL install
  *    
  * Optional Environment:
  *    PATH_PREFIX        - prefix to append to all paths read from db. used for testing only
+ *    TESSERACT_HOME     - base directory of tesseract install
+
  *    
  * @author loufoster
  *
  */
 public class EmopController {
+    public enum Algorithm {JUXTA, LEVENSHTEIN, JARO_WINKLER, DICE_SORENSEN};
+    
     private Database db;
     private long timeLeftMs;
     private long wallTimeSec;
     private String juxtaHome;
+    private String tesseractHome;
     private String pathPrefix = "";
+    private Algorithm algorithm = Algorithm.JARO_WINKLER;
     
     private static Logger LOG = Logger.getLogger(EmopController.class);
     private static final long JX_TIMEOUT_MS = 1000*60*2;    //2 mins
     
-    // TODO add command line processing to handle console, juxtaCL algorithm, walltime, prefix, etc
-
+    private static final String version = "1.0-SNAPSHOT";
+    
+    /**
+     * Main entry point for the controller
+     * @param args
+     */
     public static void main(String[] args) {        
-        boolean consoleLog =  ( args.length == 1 && args[0].equals("-console") );
         try {
-            initLogging( consoleLog );
-
             EmopController emop = new EmopController();
-            emop.init();
-            emop.killStalledJobs();
-            emop.doWork();
-            emop.shutdown();
+            if ( emop.init( args ) ) {
+                emop.killStalledJobs();
+                emop.doWork();
+                emop.shutdown();
+            }
         } catch ( SQLException e) {
             System.err.println("eMOP Controller database error");
             e.printStackTrace();
@@ -76,7 +95,7 @@ public class EmopController {
      * @param consoleLogger
      * @throws IOException
      */
-    protected static void initLogging( boolean consoleLogger ) throws IOException {
+    private void initLogging( boolean consoleLogger ) throws IOException {
         LogManager.getRootLogger().removeAllAppenders();
         LogManager.getRootLogger().setLevel(Level.DEBUG);
         if ( consoleLogger == false ) {
@@ -95,11 +114,139 @@ public class EmopController {
     /**
      * Initialize the controller. Get run settings from environment. Init database connection.
      * @throws SQLException 
+     * @throws OptionException 
      * @throws FileNotFoundException 
      */
-    public void init() throws IOException, SQLException {
-        LOG.info("Initialize eMOP controller");
+    public boolean init( String[] args ) throws IOException, SQLException, OptionException {
+                   
+        // pull some cfg from environment settings
+        getEnvironmentConfig();
         
+        // parse the commandline settings
+        final ArgumentBuilder aBuilder = new ArgumentBuilder();
+        final DefaultOptionBuilder oBuilder = new DefaultOptionBuilder();
+        final GroupBuilder gBuilder = new GroupBuilder();
+        
+        Option version = oBuilder
+            .withShortName("version")
+            .create();
+        Option help = oBuilder
+            .withShortName("help")
+            .create();
+        Option consoleLog = oBuilder
+            .withShortName("console")
+            .create();
+        
+        // out directory
+        Argument outArg = aBuilder.withMinimum(1).withMaximum(1).withName("path").create();
+        Option out = oBuilder
+            .withShortName("out")
+            .withArgument(outArg )
+            .create();
+        
+        // algorithm
+        Set<String> diffSet = new TreeSet<String>();
+        diffSet.add("juxta");
+        diffSet.add("levenshtein");
+        diffSet.add("jaro_winkler");
+        diffSet.add("dice_sorensen");
+        EnumValidator algoVal = new EnumValidator( diffSet );
+        Option algo = oBuilder
+          .withShortName("algorithm")
+          .withArgument(
+              aBuilder
+                  .withMinimum(0)
+                  .withMaximum(1)
+                  .withDefault("juxta")
+                  .withValidator(algoVal)
+                  .create())
+          .create();
+        
+        Group jobOpts = gBuilder.withOption(consoleLog).withOption(out).withOption(algo).create(); 
+        
+        // main param: job wall time
+        Argument wallTimeArg = aBuilder.withMinimum(1).withMaximum(1).withName("time").create();
+        Option job = oBuilder
+            .withShortName("wall")
+            .withArgument(wallTimeArg )
+            .withChildren(jobOpts)
+            .create();
+
+        
+        // glom all of the options together into the main grouping. it will be used for the parse.
+        Group opts = gBuilder
+            .withOption(job)
+            .withOption(help)
+            .withOption(version).create();
+
+        // parse the options passed in
+        Parser parser = new Parser();
+        parser.setGroup(opts);
+        CommandLine cl = parser.parse(args);
+       
+        if ( cl.hasOption(version)) {
+            System.out.println("eMOP Controller Version "+EmopController.version);
+            return false;
+        } 
+        
+        if ( cl.hasOption(help)) { 
+            showHelp();
+            return false;
+        } 
+        
+        boolean logToConsole = cl.hasOption(consoleLog);
+        initLogging(logToConsole) ;
+        
+        if ( cl.hasOption(algo)) {
+            String a = (String)cl.getValue(algo);
+            this.algorithm = Algorithm.valueOf(a.toUpperCase());
+        }
+        
+        if ( cl.hasOption(job)) {
+            String timeStr = (String)cl.getValue(job);
+            if ( timeStr != null && timeStr.length() > 0 ) {
+                this.wallTimeSec = Integer.parseInt(timeStr);
+            }
+        }        
+        this.timeLeftMs = this.wallTimeSec*1000;
+
+        if ( cl.hasOption(out)) {
+            String outVal = (String)cl.getValue(out);
+            if ( outVal != null && outVal.length() > 0 ) {
+                this.pathPrefix = outVal;
+            }
+        }
+
+        LOG.info("Initialize eMOP controller");
+        initDatabase();
+        return true;
+    }
+    
+    private void getEnvironmentConfig() {
+        this.juxtaHome =  System.getenv("JUXTA_HOME");
+        if ( this.juxtaHome == null || this.juxtaHome.length() == 0) {
+            throw new RuntimeException("Missing require JUXTA_HOME environment variable");
+        }
+        
+        String th =  System.getenv("TESSERACT_HOME");
+        if ( th != null && th.length() > 0) {
+            this.tesseractHome = th;
+        }
+    }
+
+    private void showHelp() {
+        StringBuilder out = new StringBuilder("Usage: emop wall [timeSec] [options]\n");
+        out.append("Where wall defines process lifespan in seconds.");
+        out.append("  Options:\n");
+        out.append("    out        : Path to root directory for output.\n");
+        out.append("    algorithm  : Algorithm used by JuxtaCL to determine change index.\n");
+        out.append("                   Options:\n");
+        out.append("                     juxta, levenshtein, dice_sorensen jaro_winkler\n");
+        out.append("                   Defaults to jaro_winkler\n");
+        System.out.println(out.toString());
+    }
+
+    private void initDatabase() throws IOException, SQLException {
         // pull settings .my.cf
         Properties mySql = loadMySqlCfg();
         String dbHost = mySql.getProperty("host");
@@ -116,31 +263,10 @@ public class EmopController {
         if ( dbPass == null || dbPass.length() == 0) {
             dbPass = "";
         }
-        
-        // Get other runtime config from environment
-        String strWorkTimeSec =  System.getenv("EMOP_WALLTIME");
-        this.wallTimeSec = 600;
-        if ( strWorkTimeSec != null && strWorkTimeSec.length() > 0) {
-            this.wallTimeSec = Integer.parseInt(strWorkTimeSec);
-        }
-        this.timeLeftMs = this.wallTimeSec*1000;
-        
-        this.juxtaHome =  System.getenv("JUXTA_HOME");
-        if ( this.juxtaHome == null || this.juxtaHome.length() == 0) {
-            throw new RuntimeException("Missing require JUXTA_HOME environment variable");
-        }
-             
-        // optional path prefix for testing modes
-        String prefix =  System.getenv("PATH_PREFIX");
-        if ( prefix != null && prefix.length() > 0) {
-            this.pathPrefix = prefix;
-        }
-
-        // connect to database
         this.db = new Database();
         this.db.connect(dbHost, "emop", dbUser, dbPass);
     }
-    
+
     private Properties loadMySqlCfg() throws IOException {
         File home = new File(System.getenv("HOME"));
         File myCnf = new File(home, ".my.cnf");
@@ -274,7 +400,7 @@ public class EmopController {
         // kickoff the OCR engine and wait until it completes
         // NOTE: strip the .txt extension; tesseract auto-appends it
         ProcessBuilder pb = new ProcessBuilder(
-            "/usr/local/bin/tesseract", pageImage.getImagePath(), outFile.replaceAll(".txt", ""));
+            this.tesseractHome+"/tesseract", pageImage.getImagePath(), outFile.replaceAll(".txt", ""));
         Process jxProc = pb.start();
         awaitProcess(jxProc, JX_TIMEOUT_MS);
         if (jxProc.exitValue() != 0) {
@@ -301,7 +427,7 @@ public class EmopController {
                 "java", "-jar", "-server", 
                 this.juxtaHome+"/juxta-cl.jar", "-diff",
                 addPrefix(gtPath),addPrefix(ocrPath), 
-                "-algorithm", "jaro_winkler");
+                "-algorithm", this.algorithm.toString().toLowerCase());
             pb.directory( new File(this.juxtaHome) );
             Process jxProc = pb.start();
             awaitProcess(jxProc, JX_TIMEOUT_MS);
