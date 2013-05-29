cbb66fcce9df9e564fd261ceb4fd044caf17b243
Lou Foster <lou@performantsoftware.com>
2013-05-29 16:19:38 -0400

    revised view

--- scripts/create_view.sql
+++ scripts/create_view.sql
@@ -1,27 +1,11 @@
 --
--- Create a view that averages the most recent batch run result for non-gale OCR
+-- Create a view that averages the page results per batch
 --
 create view work_ocr_results as
    select pg_work_id as work_id, 
-          batch_id,
-          ocr_completed,
+          page_results.batch_id as batch_id,
           ocr_engine_id,
           avg(juxta_change_index) as juxta_accuracy, 
           avg(alt_change_index) as retas_accuracy
-      from pages, page_results,batch_job  where pg_page_id=page_id and batch_id=batch_job.id and batch_id = 
-      (select batch_id from pages, page_results, batch_job  
-        where pg_page_id=page_id and batch_job.id=batch_id and ocr_engine_id!=1 
-        order by ocr_completed desc limit 1);
-	  
---
--- Create a view that averages the gale results
---
-create view work_gale_results as
-	select pg_work_id as work_id, 
-          batch_id,
-	       avg(juxta_change_index) as juxta_accuracy, 
-	       avg(alt_change_index) as retas_accuracy
-   from pages, page_results  where pg_page_id=page_id and batch_id = 
-	   (select batch_id from pages, page_results, batch_job  
-	     where pg_page_id=page_id and batch_job.id=batch_id and ocr_engine_id=1 
-	     order by ocr_completed desc limit 1);
+      from pages, page_results, batch_job where pg_page_id=page_id and batch_id=batch_job.id 
+      group by batch_id;
