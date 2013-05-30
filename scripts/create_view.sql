e3017d85e772112a45b45d4a45322d596096539f
Lou Foster <lou@performantsoftware.com>
2013-05-30 12:51:14 -0400

    updated view

--- scripts/create_view.sql
+++ scripts/create_view.sql
@@ -3,9 +3,12 @@
 --
 create view work_ocr_results as
    select pg_work_id as work_id, 
+          ocr_completed,
           page_results.batch_id as batch_id,
+          batch_job.name as batch_name,
           ocr_engine_id,
           avg(juxta_change_index) as juxta_accuracy, 
           avg(alt_change_index) as retas_accuracy
-      from pages, page_results, batch_job where pg_page_id=page_id and batch_id=batch_job.id 
+      from pages, page_results, batch_job 
+      where pg_page_id=page_id and batch_id=batch_job.id
       group by batch_id;
