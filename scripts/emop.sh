a4cecdcf27be6b0b869b244a4651c9aa15c1d2be
Lou Foster <lou@performantsoftware.com>
2013-07-09 10:57:18 -0400

    cleaned up formatting

--- scripts/emop.sh
+++ scripts/emop.sh
@@ -14,24 +14,24 @@ Q_STATUS=$($CMD)
 # only allow one emop controler to be schedulated at a time
 if [[ "$Q_STATUS" == *$APP_NAME* ]]
 then
-  echo "An instance of ${APP_NAME} is already running."
-  exit 1
+   echo "An instance of ${APP_NAME} is already running."
+   exit 1
 else
-  # ensure that there is work to do before scheduling
-  JOB_CNT=$(env EMOP_HOME=$EMOP_HOME java -Xms128M -Xmx128M -jar emop-controller.jar -check)
-  CODE=$?
-  if [ $CODE -ne 0 ];
-  then
-    # do not submit a new controller if there were  errors checking count
-    echo "Unable to determine job count. Not launching eMOP controller"
-  else
-    if [ $JOB_CNT -gt 0 ];
-	 then
-	   # there is work in the emop job_queue. Schedule the controller
-	   # to process these jobs
-      qsub emop.pbs
-    fi
-  fi
+   # ensure that there is work to do before scheduling
+   JOB_CNT=$(env EMOP_HOME=$EMOP_HOME java -Xms128M -Xmx128M -jar emop-controller.jar -check)
+   CODE=$?
+   if [ $CODE -ne 0 ];
+   then
+      # do not submit a new controller if there were  errors checking count
+      echo "Unable to determine job count. Not launching eMOP controller"
+   else
+      if [ $JOB_CNT -gt 0 ];
+      then
+         # there is work in the emop job_queue. Schedule the controller
+         # to process these jobs
+         qsub emop.pbs
+      fi
+   fi
 fi
 
 exit 0
